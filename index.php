05ad67aa3a96db5be3e0a1d2251f5be467d615df
Paundra <h1101241029@student.untan.ac.id>
2025-08-28 22:52:27 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// index.php
+$nama = " Atmin Mie ayam";
+$waktu = date("Y-m-d H:i:s");
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Website PHP di Hugging Face</title>
+    <style>
+        body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
+        .container { max-width: 600px; margin: 0 auto; }
+        .box { background: #f0f8ff; padding: 20px; border-radius: 10px; margin-top: 20px; }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>WELCOME TO MIE AYAM WEBSITE</h1>
+        <p>Semoga dengan adanya wesbite ini para mie ayam diseluruh dunia kesejahteraannya meningkat dan menjadi lebih baiik <strong><?= htmlspecialchars($nama) ?></strong></p>
+        <div class="box">
+            <p>Waktu server: <code><?= $waktu ?></code></p>
+            <p>Dijalankan di <strong>Docker</strong> di Hugging Face Spaces ✅</p>
+            
+        </div>
+  </div>
+</body>
+</html>
