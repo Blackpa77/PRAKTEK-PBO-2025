4fc2be15125098f287c672c37ec09c6fd0a5d2aa
Blackpa77 <h1101241029@student.untan.ac.id>
2025-10-02 20:47:40 +0700

    used branch 'main'

--- Pertemuan_Tujuh_Latihan/src/Models/Media.php
+++ Pertemuan_Tujuh_Latihan/src/Models/Media.php
@@ -1,28 +1,19 @@
 <?php
 namespace Models;
-// Konsep: Abstract Class, Inheritance, Scope, Class Constants, Final, Magic Methods
+
 abstract class Media {
     protected string $title;
-    private int $id;
-    protected static int $counter = 0;
-    const MEDIA_TYPE = 'Generic';
 
     public function __construct(string $title) {
         $this->title = $title;
-        $this->id = ++self::$counter;
-        echo "<i>(Info: Objek Media baru dibuat)</i><br>";
+
     }
+
     abstract public function getDetails(): string;
+
     public function getTitle(): string {
         return $this->title;
     }
-    public static function getMediaType(): string {
-        return static::MEDIA_TYPE; // Late Static Binding
-    }
-    final public function getId(): int {
-        return $this->id;
-    }
-    public function __destruct() {
-        echo "<i>(Info: Objek Media '{$this->title}' dihapus)</i><br>";
-    }
+
+    // METHOD __DESTRUCT JUGA DIHAPUS KARENA MENYEBABKAN OUTPUT
 }
