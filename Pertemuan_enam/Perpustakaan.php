2a3be00e710cf36753ad9e73201d44337cba72a9
Blackpa77 <h1101241029@student.untan.ac.id>
2025-10-21 07:37:11 +0700

    update

--- Pertemuan_enam/Perpustakaan.php
+++ Pertemuan_enam/Perpustakaan.php
@@ -1,5 +1,6 @@
 <?php
 
+// --- BAGIAN 1: DEFINISI TRAITS (Kemampuan tambahan) ---
 trait Loggable {
     public function logActivity($message) {
         echo "[LOG - " . date("Y-m-d H:i:s") . "]: " . $message . "\n";
@@ -22,7 +23,10 @@ trait Searchable {
     }
 }
 
+
+// --- BAGIAN 2: CLASS INDUK (Parent Class) ---
 class ItemPerpustakaan {
+    // Menggunakan traits
     use Loggable, Searchable;
 
     protected $judul;
@@ -57,10 +61,18 @@ class ItemPerpustakaan {
             $this->logActivity("Item '{$this->judul}' telah dikembalikan.");
         }
     }
+    
+    public function getDetail() {
+        return "Judul: {$this->judul}, Penulis: {$this->penulis}, Tahun: {$this->tahunTerbit}, Status: {$this->status}";
+    }
 }
 
+
+// --- BAGIAN 3: CLASS ANAK (Child Classes) ---
+
+// Class Buku mewarisi dari ItemPerpustakaan
 class Buku extends ItemPerpustakaan {
-    private $isbn;
+    private $isbn; // Properti spesifik
 
     public function __construct($judul, $penulis, $tahunTerbit, $isbn) {
         parent::__construct($judul, $penulis, $tahunTerbit);
@@ -71,10 +83,16 @@ class Buku extends ItemPerpustakaan {
         echo "--> Memproses peminjaman untuk BUKU '{$this->judul}'...\n";
         return parent::pinjam();
     }
+
+    // Override method untuk tambah info spesifik
+    public function getDetail() {
+        return parent::getDetail() . ", ISBN: {$this->isbn}";
+    }
 }
 
+// Class DVD mewarisi dari ItemPerpustakaan
 class DVD extends ItemPerpustakaan {
-    private $durasi;
+    private $durasi; // Properti spesifik
 
     public function __construct($judul, $penulis, $tahunTerbit, $durasi) {
         parent::__construct($judul, $penulis, $tahunTerbit);
@@ -85,10 +103,15 @@ class DVD extends ItemPerpustakaan {
         echo "--> Memproses peminjaman untuk DVD '{$this->judul}'...\n";
         return parent::pinjam();
     }
+
+    // Override method untuk tambah info spesifik
+    public function getDetail() {
+        return parent::getDetail() . ", Durasi: {$this->durasi} menit";
+    }
 }
 
 class Majalah extends ItemPerpustakaan {
-    private $edisi;
+    private $edisi; // Properti spesifik
 
     public function __construct($judul, $penulis, $tahunTerbit, $edisi) {
         parent::__construct($judul, $penulis, $tahunTerbit);
@@ -99,10 +122,16 @@ class Majalah extends ItemPerpustakaan {
         echo "--> Memproses peminjaman untuk MAJALAH '{$this->judul}'...\n";
         return parent::pinjam();
     }
+    
+    public function getDetail() {
+        return parent::getDetail() . ", Edisi: {$this->edisi}";
+    }
 }
 
+
+// --- BAGIAN 4: CLASS LAIN PENGGUNA TRAITS ---
 class Pengguna {
-    use Notifiable;
+    use Notifiable; // Pengguna bisa dinotifikasi
     public $nama;
 
     public function __construct($nama) {
@@ -111,7 +140,7 @@ class Pengguna {
 }
 
 class Transaksi {
-    use Loggable, Notifiable;
+    use Loggable, Notifiable; // Transaksi bisa log dan notifikasi
 
     public function prosesPeminjaman(Pengguna $pengguna, ItemPerpustakaan $item) {
         $this->logActivity("Transaksi peminjaman dimulai oleh {$pengguna->nama} untuk item '{$item->getJudul()}'.");
@@ -124,10 +153,18 @@ class Transaksi {
     }
 }
 
+
+// --- BAGIAN 5: EKSEKUSI PROGRAM ---
+
+// Tag <pre> untuk merapikan output di browser
+echo "<pre>";
+
 $bukuPemrograman = new Buku("Belajar PHP & MySQL", "Andi Setiawan", 2023, "978-602-04-9893-7");
 $dvdFilm = new DVD("The Last of Us: Season 1", "Neil Druckmann", 2023, 520);
+$majalahSains = new Majalah("National Geographic", "Various", 2023, "Oktober 2023");
 $pengguna = new Pengguna("Mahasiswa Untan");
 $transaksi = new Transaksi();
+
 
 echo "========================================\n";
 echo "### SKENARIO 1: Peminjaman Berhasil ###\n";
@@ -145,5 +182,16 @@ echo "=======================================================\n";
 $bukuPemrograman->searchByTitle("PHP");
 $dvdFilm->searchByTitle("PHP");
 $dvdFilm->searchByTitle("Last of Us");
+
+echo "\n=======================================================\n";
+echo "### SKENARIO 4: Menampilkan Properti Spesifik (ISBN, Durasi, dll) ###\n";
+echo "=======================================================\n";
+// Memanggil method getDetail() yang sudah di-override
+echo "Detail Buku   : " . $bukuPemrograman->getDetail() . "\n";
+echo "Detail DVD    : " . $dvdFilm->getDetail() . "\n";
+echo "Detail Majalah: " . $majalahSains->getDetail() . "\n";
+
+
+echo "</pre>";
 
 ?>
